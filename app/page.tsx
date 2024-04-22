81c7241c57eaad239c2a5972d09b3440178d9f95
daniel.theo <daniel.theo@javan.co.id>
2024-04-22 08:42:24 +0700

    ✨feature: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,107 @@
-import Image from "next/image";
+// import { StemmerEn, StopwordsEn } from "@nlpjs/lang-en";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
+import { NowPlaying } from "./component/now_playing";
+import { Popular } from "./component/popular";
+import { Upcoming } from "./component/upcoming";
+import { api } from "@/lib/utils";
+import { IMovie, IMovieList } from "@/lib/type";
+async function getNowPlaying() {
+  let currentPage = 1;
+  let maxPage = 0;
+  const data: IMovie[] = [];
 
-export default function Home() {
+  do {
+    const url = `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${currentPage}&region=ID`;
+    const { results: movies, total_pages }: IMovieList = await api.fetch(url);
+
+    movies.map(async (movie) => {
+      data.push(movie);
+    });
+
+    maxPage = total_pages;
+    currentPage++;
+  } while (currentPage <= maxPage);
+
+  return data;
+}
+
+async function getPopular() {
+  const data: IMovie[] = [];
+
+  const url = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1region=ID`;
+  const { results: movies }: IMovieList = await api.fetch(url);
+
+  movies.map((movie) => {
+    data.push(movie);
+  });
+
+  return data;
+}
+
+async function getUpcoming() {
+  let currentPage = 1;
+  let maxPage = 0;
+  const data: IMovie[] = [];
+
+  do {
+    const url = `https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=${currentPage}&region=ID`;
+    const { results: movies, total_pages }: IMovieList = await api.fetch(url);
+
+    movies.map((movie) => {
+      data.push(movie);
+    });
+
+    maxPage = total_pages;
+    currentPage++;
+  } while (currentPage <= maxPage);
+
+  return data;
+}
+
+export default async function Home() {
+  // const stemmer = new StemmerEn();
+  // stemmer.stopwords = new StopwordsEn();
+  // const input = "asdf";
+  // console.log(stemmer.tokenizeAndStem(input, false));
+  const dataNowPlaying = await getNowPlaying();
+  const dataPopular = await getPopular();
+  const dataUpcoming = await getUpcoming();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="min-h-screen">
+      <nav className="text-xl absolute z-20 p-16 w-full text-center">
+        CINE<span className="text-primary">MATCH</span>
+      </nav>
+      <div
+        className="w-full bg-cover bg-center flex flex-col items-center justify-center relative z-10 before:content-[''] before:absolute before:inset-0 before:bg-gradient-to-t before:from-background before:from-5% before:via-transparent before:to-95% before:to-background before:z-[-5] after:content-[''] after:absolute after:inset-0 after:bg-gradient-to-r after:from-background after:via-transparent after:to-background after:z-[-5]"
+        style={{
+          backgroundImage:
+            "linear-gradient(to right, hsl(var(--background) / .3), hsl(var(--background) / .3)), url(background.jpg)",
+          height: "100vh",
+        }}
+      >
+        <div className="p-24 w-1/2 max-w-screen-lg flex flex-col gap-5">
+          <p className="text-5xl text-center font-semibold">
+            <span className="text-primary">Unlock</span> Your Perfect Movie
+            Match with Cine<span className="text-primary">match!</span>
+          </p>
+          <div className="flex flex-row gap-2">
+            <Input
+              className="bg-background"
+              type="email"
+              placeholder="Keyword"
             />
-          </a>
+            <Button>
+              <Search className="size-4 me-2" /> Search
+            </Button>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <NowPlaying data={dataNowPlaying} />
+      <Popular data={dataPopular} />
+      <Upcoming data={dataUpcoming} />
     </main>
   );
 }
