d57e745090c4052e713d0c8e93ec48b750c22de6
daniel.theo <daniel.theo@javan.co.id>
2024-05-08 14:58:32 +0700

    🔵 other: refactor

--- components/main/card_image.tsx
+++ components/main/card_image.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { cn } from "@/lib/utils";
 
 interface ICardImage {
-  posterPath: string;
+  posterPath: string | null;
   alt: string;
   defaultImage?: string;
   className?: string;
