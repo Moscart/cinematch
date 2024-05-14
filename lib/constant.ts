84099a7a3084fff9d542cdf4121e953ad294e971
daniel.theo <daniel.theo@javan.co.id>
2024-05-14 12:38:36 +0700

    🔵 other: add constant message

--- lib/constant.ts
+++ lib/constant.ts
@@ -19,3 +19,20 @@ export const GENRE_MOVIE = {
   10752: "Perang",
   37: "Barat",
 };
+
+type MessageNotFound = {
+  [key: number]: string;
+};
+
+export const MESSAGE_NOT_FOUND: MessageNotFound = {
+  1: "Maaf, kami tidak menemukan film dengan kata kunci tersebut. Coba gunakan kata kunci yang berbeda atau periksa kembali pengetikannya.",
+  2: "Hmm, sepertinya tidak ada film yang cocok dengan kata kunci yang Anda cari. Silakan coba dengan kata kunci lain.",
+  3: "Maaf, tidak ada hasil untuk kata kunci yang Anda masukkan. Coba gunakan kata kunci yang lebih umum atau periksa ejaannya.",
+  4: "Ups! Tidak ada film yang sesuai dengan kata kunci yang Anda cari. Silakan coba dengan kata kunci yang berbeda.",
+  5: "Kami tidak dapat menemukan film yang cocok dengan kata kunci tersebut. Silakan periksa kembali kata kunci atau coba gunakan yang lain.",
+  6: "Mohon maaf, kami tidak menemukan hasil film untuk kata kunci yang Anda masukkan. Coba ganti kata kunci atau gunakan kata yang lebih umum.",
+  7: "Tidak ada film yang cocok dengan kata kunci Anda. Cobalah gunakan kata kunci lain atau periksa ejaannya.",
+  8: "Hasil tidak ditemukan untuk kata kunci yang Anda masukkan. Mungkin Anda ingin mencoba kata kunci yang berbeda?",
+  9: "Maaf, tidak ada film yang cocok dengan pencarian Anda. Coba gunakan kata kunci lain atau periksa kembali ejaannya.",
+  10: "Kami tidak menemukan film berdasarkan kata kunci Anda. Mungkin cobalah kata kunci lain atau gunakan istilah yang lebih umum.",
+};
