1091216df6652c853ba2881cbc3a33879e71e4db
daniel.theo <daniel.theo@javan.co.id>
2024-05-19 10:06:19 +0700

    🔵 other: refactor

--- app/page.tsx
+++ app/page.tsx
@@ -91,8 +91,8 @@ export default async function Home() {
                 placeholder="Kata Kunci"
                 autoComplete="off"
               />
-              <Button>
-                <Search className="size-4 me-2" /> Cari
+              <Button className="text-background">
+                <Search className="size-4 me-2" strokeWidth={3} /> Cari
               </Button>
             </div>
           </form>
