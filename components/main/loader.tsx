c248fd4a7efad27e825d543647b127c517850e87
daniel.theo <daniel.theo@javan.co.id>
2024-05-19 12:31:11 +0700

    ✨feature: make loading

---
+++ components/main/loader.tsx
@@ -0,0 +1,13 @@
+export default function Loader() {
+  return (
+    <div className="flex gap-5 justify-center items-center">
+      <span className="scaling-letter">L</span>
+      <span className="scaling-letter">O</span>
+      <span className="scaling-letter">A</span>
+      <span className="scaling-letter">D</span>
+      <span className="scaling-letter">I</span>
+      <span className="scaling-letter">N</span>
+      <span className="scaling-letter">G</span>
+    </div>
+  );
+}
