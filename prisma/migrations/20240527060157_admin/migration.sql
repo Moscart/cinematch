86d7c290ace0d7acb928a1f14f27c5c64622e9bd
daniel.theo <daniel.theo@javan.co.id>
2024-05-27 13:40:50 +0700

    ✨feature: add admin on database

---
+++ prisma/migrations/20240527060157_admin/migration.sql
@@ -0,0 +1,44 @@
+/*
+  Warnings:
+
+  - You are about to drop the `account` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `authenticator` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `session` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `verificationtoken` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `account` DROP FOREIGN KEY `Account_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `authenticator` DROP FOREIGN KEY `Authenticator_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `session` DROP FOREIGN KEY `Session_userId_fkey`;
+
+-- DropTable
+DROP TABLE `account`;
+
+-- DropTable
+DROP TABLE `authenticator`;
+
+-- DropTable
+DROP TABLE `session`;
+
+-- DropTable
+DROP TABLE `user`;
+
+-- DropTable
+DROP TABLE `verificationtoken`;
+
+-- CreateTable
+CREATE TABLE `Admin` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Admin_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
