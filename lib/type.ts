d531c48530d6d61ce99cc489566e6cce3fd88135
daniel.theo <daniel.theo@javan.co.id>
2024-05-27 11:36:46 +0700

    ✨feature: add interface for detail movie

--- lib/type.ts
+++ lib/type.ts
@@ -34,3 +34,165 @@ export interface IStringLimiter {
   text: string;
   length: number;
 }
+
+export interface MovieDetail {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: null;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  origin_country: OriginCountry[];
+  original_language: OriginalLanguage;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: Date;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  credits: Credits;
+  videos: Videos;
+  images: Images;
+}
+
+export interface Credits {
+  cast: Cast[];
+  crew: Cast[];
+}
+
+export interface Cast {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: Department;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: null | string;
+  cast_id?: number;
+  character?: string;
+  credit_id: string;
+  order?: number;
+  department?: Department;
+  job?: string;
+}
+
+export interface CrewDouble {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: Department;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: null | string;
+  cast_id?: number;
+  character?: string;
+  credit_id: string;
+  order?: number;
+  department?: Department;
+  job?: string[];
+}
+
+export enum Department {
+  Acting = "Acting",
+  Art = "Art",
+  Camera = "Camera",
+  CostumeMakeUp = "Costume & Make-Up",
+  Crew = "Crew",
+  Directing = "Directing",
+  Editing = "Editing",
+  Lighting = "Lighting",
+  Production = "Production",
+  Sound = "Sound",
+  VisualEffects = "Visual Effects",
+  Writing = "Writing",
+}
+
+export interface Genre {
+  id: GenreId;
+  name: string;
+}
+
+export interface Images {
+  backdrops: Backdrop[];
+  logos: Backdrop[];
+  posters: Backdrop[];
+}
+
+export interface Backdrop {
+  aspect_ratio: number;
+  height: number;
+  iso_639_1: null | string;
+  file_path: string;
+  vote_average: number;
+  vote_count: number;
+  width: number;
+}
+
+export enum OriginCountry {
+  Us = "US",
+}
+
+export enum OriginalLanguage {
+  En = "en",
+}
+
+export interface ProductionCompany {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+export interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface Videos {
+  results: Result[];
+}
+
+export interface Result {
+  iso_639_1: OriginalLanguage;
+  iso_3166_1: OriginCountry;
+  name: string;
+  key: string;
+  site: Site;
+  size: number;
+  type: Type;
+  official: boolean;
+  published_at: Date;
+  id: string;
+}
+
+export enum Site {
+  YouTube = "YouTube",
+}
+
+export enum Type {
+  BehindTheScenes = "Behind the Scenes",
+  Clip = "Clip",
+  Featurette = "Featurette",
+  Teaser = "Teaser",
+  Trailer = "Trailer",
+}
