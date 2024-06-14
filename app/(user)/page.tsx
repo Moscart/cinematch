4ed94e33f1b5c875683ffb61f3eda845655e0112
daniel.theo <daniel.theo@javan.co.id>
2024-06-14 12:42:40 +0700

    ✨feature: add background and parallax to home page

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,13 +1,10 @@
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Search } from "lucide-react";
 import { NowPlaying } from "./components/now_playing";
 import { Popular } from "./components/popular";
 import { Upcoming } from "./components/upcoming";
 import { api } from "@/lib/utils";
 import { IMovie, IMovieList } from "@/lib/type";
-import { redirect } from "next/navigation";
-import Form from "./form";
+import Background from "@/components/main/background";
+import Parallax from "./components/parallax";
 async function getNowPlaying() {
   let currentPage = 1;
   let maxPage = 0;
@@ -65,28 +62,10 @@ export default async function Home() {
   const dataNowPlaying = await getNowPlaying();
   const dataPopular = await getPopular();
   const dataUpcoming = await getUpcoming();
-
   return (
     <main className="min-h-screen">
-      <nav className="text-xl absolute z-20 p-16 w-full text-center">
-        CINE<span className="text-primary">MATCH</span>
-      </nav>
-      <div
-        className="w-full bg-cover bg-center flex flex-col items-center justify-center relative z-10 before:content-[''] before:absolute before:inset-0 before:bg-gradient-to-t before:from-background before:from-5% before:via-transparent before:to-95% before:to-background before:z-[-5] after:content-[''] after:absolute after:inset-0 after:bg-gradient-to-r after:from-background after:via-transparent after:to-background after:z-[-5]"
-        style={{
-          backgroundImage:
-            "linear-gradient(to right, hsl(var(--background) / .3), hsl(var(--background) / .3)), url(background.jpg)",
-          height: "100vh",
-        }}
-      >
-        <div className="p-24 w-1/2 max-w-screen-lg flex flex-col gap-5">
-          <p className="text-5xl text-center font-semibold">
-            <span className="text-primary">Temukan</span> Cerita yang Sesuai
-            denganmu di Cine<span className="text-primary">match!</span>
-          </p>
-          <Form />
-        </div>
-      </div>
+      <Background />
+      <Parallax />
       <NowPlaying data={dataNowPlaying} />
       <Popular data={dataPopular} />
       <Upcoming data={dataUpcoming} />
