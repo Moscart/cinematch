a2159e9d6be0fe89a15849433517a620d072a7c5
daniel.theo <daniel.theo@javan.co.id>
2024-06-14 12:41:55 +0700

    🔵 other: refactor

--- app/(user)/components/parallax.tsx
+++ app/(user)/components/parallax.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Parallax() {
   return (
-    <div className="mt-12">
+    <div className="">
       <ParallaxText baseVelocity={5}>
         CINE<div className="inline-block text-primary me-2">MATCH</div>
       </ParallaxText>
