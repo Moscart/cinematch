8168033ec2e0473d9d41ea1ee0d8ac1729b53108
daniel.theo <daniel.theo@javan.co.id>
2024-06-18 11:49:34 +0700

    🔵 other: Refactor Rekomendasi page: update imports, add new icon, modify layout and error messages, and move MESSAGE_NOT_FOUND constant to page component

--- app/(user)/rekomendasi/page.tsx
+++ app/(user)/rekomendasi/page.tsx
@@ -10,41 +10,57 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { MESSAGE_NOT_FOUND } from "@/lib/constant";
 import { recommendation } from "@/lib/serverUtils";
 import { Movie } from "@prisma/client";
-import { AlertCircle, ChevronLeft, Loader, Search } from "lucide-react";
+import { AlertCircle, Clapperboard, Loader, Search } from "lucide-react";
 import moment from "moment";
 import "moment/locale/id";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { FormEvent, Suspense, useEffect, useState } from "react";
-import { animatePageOut } from "../animation";
 
 function Rekomendasi() {
   const [data, setData] = useState<Movie[] | []>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
+  const [random, setRandom] = useState<number>(0);
   const router = useRouter();
   const searchParams = useSearchParams();
-  const keywords = `${searchParams.get("keywords")}`;
+  const keywords = searchParams.get("keywords");
+  const [isKeyword, setIsKeyword] = useState<boolean>(
+    searchParams.has("keywords")
+  );
 
-  const generateRandomNumber = () => {
-    return Math.floor(Math.random() * 10) + 1;
-  };
-
-  const randomNumber = generateRandomNumber();
+  const MESSAGE_NOT_FOUND = [
+    "Maaf, kami tidak menemukan film dengan kata kunci tersebut. Coba gunakan kata kunci yang berbeda atau periksa kembali pengetikannya.",
+    "Hmm, sepertinya tidak ada film yang cocok dengan kata kunci yang Anda cari. Silakan coba dengan kata kunci lain.",
+    "Maaf, tidak ada hasil untuk kata kunci yang Anda masukkan. Coba gunakan kata kunci yang lebih umum atau periksa ejaannya.",
+    "Ups! Tidak ada film yang sesuai dengan kata kunci yang Anda cari. Silakan coba dengan kata kunci yang berbeda.",
+    "Kami tidak dapat menemukan film yang cocok dengan kata kunci tersebut. Silakan periksa kembali kata kunci atau coba gunakan yang lain.",
+    "Mohon maaf, kami tidak menemukan hasil film untuk kata kunci yang Anda masukkan. Coba ganti kata kunci atau gunakan kata yang lebih umum.",
+    "Tidak ada film yang cocok dengan kata kunci Anda. Cobalah gunakan kata kunci lain atau periksa ejaannya.",
+    "Hasil tidak ditemukan untuk kata kunci yang Anda masukkan. Mungkin Anda ingin mencoba kata kunci yang berbeda?",
+    "Maaf, tidak ada film yang cocok dengan pencarian Anda. Coba gunakan kata kunci lain atau periksa kembali ejaannya.",
+    "Kami tidak menemukan film berdasarkan kata kunci Anda. Mungkin cobalah kata kunci lain atau gunakan istilah yang lebih umum.",
+  ];
 
   useEffect(() => {
-    setLoading(true);
-    recommendation(keywords)
-      .then((res) => {
-        setData(JSON.parse(res));
-      })
-      .catch(() => setError(true))
-      .finally(() => {
-        setLoading(false);
-      });
+    if (searchParams.has("keywords")) {
+      setIsKeyword(true);
+      setError(false);
+      setLoading(true);
+      recommendation(keywords ?? "")
+        .then((res) => {
+          setData(JSON.parse(res));
+        })
+        .catch(() => {
+          setRandom(Math.floor(Math.random() * MESSAGE_NOT_FOUND.length));
+          setError(true);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
   }, [keywords]);
 
   const onSearch = (event: FormEvent<HTMLFormElement>) => {
@@ -55,28 +71,19 @@ function Rekomendasi() {
   };
 
   return (
-    <main className="min-h-screen">
-      <div className="p-24 relative">
-        <Button
-          size={"lg"}
-          className="absolute text-background font-bold rounded-tl-none rounded-bl-none rounded-tr-full rounded-br-full"
-          style={{
-            background:
-              "linear-gradient(to right, transparent 0%, hsl(var(--primary)) 90%)",
-          }}
-          onClick={() => animatePageOut("/", router)}
-        >
-          <ChevronLeft className="h-4 w-4 ms-5 me-2" strokeWidth={3} />
-          BACK
-        </Button>
-        <h1 className="text-center mb-24">Rekomendasi</h1>
+    <main className="">
+      <div className="p-24 pb-12">
+        <h1 className="text-center mb-12 max-w-screen-md mx-auto uppercase">
+          <span className="text-primary">Temukan</span> film yang cocok dengan{" "}
+          <span className="text-primary">selera</span> mu!
+        </h1>
         <form onSubmit={onSearch} className="max-w-screen-md m-auto">
           <div className="flex flex-row gap-2">
             <Input
               className="bg-background"
               type="text"
               name="keywords"
-              placeholder="Kata Kunci"
+              placeholder="Masukkan kata kunci"
               autoComplete="off"
             />
             <Button className="text-background font-bold">
@@ -130,8 +137,14 @@ function Rekomendasi() {
                         <p className="text-sm">{movie.title}</p>
                       </div>
                     </div>
+                    <p className="font-semibold text-lg mt-3">Genre</p>
+                    <p className="font-light text-sm">
+                      {movie.genre ? movie.genre : "Tidak ada genre"}
+                    </p>
                     <p className="font-semibold text-lg mt-3">Ringkasan</p>
-                    <p className="font-light text-sm">{movie.overview}</p>
+                    <p className="font-light text-sm">
+                      {movie.overview ? movie.overview : "Tidak ada ringkasan"}
+                    </p>
                     <p className="font-semibold text-xs mt-3">
                       {moment(movie.release_date).format("MMMM DD, YYYY")}
                     </p>
@@ -151,7 +164,7 @@ function Rekomendasi() {
           </div>
         </div>
       )}
-      {!error && !loading && !data.length && (
+      {isKeyword && !error && !loading && !data.length && (
         <div className="p-24 pt-0 flex justify-center">
           <Card className="max-w-screen-md">
             <CardHeader>
@@ -163,7 +176,7 @@ function Rekomendasi() {
               </CardTitle>
               <CardDescription>Film tidak ditemukan</CardDescription>
             </CardHeader>
-            <CardContent>{MESSAGE_NOT_FOUND[randomNumber]}</CardContent>
+            <CardContent>{MESSAGE_NOT_FOUND[random]}</CardContent>
           </Card>
         </div>
       )}
@@ -173,6 +186,27 @@ function Rekomendasi() {
             <p className="">Please Wait</p>
             <Loader className="size-4 mt-2 animate-spin" />
           </div>
+        </div>
+      )}
+      {!isKeyword && !error && !loading && (
+        <div className="p-24 pt-0 flex justify-center">
+          <Card className="max-w-screen-md">
+            <CardHeader>
+              <CardTitle>
+                <div className="flex items-center">
+                  <Clapperboard className="size-4 me-2" />
+                  Rekomendasi Film
+                </div>
+              </CardTitle>
+              <CardDescription>
+                Temukan film sesuai dengan preferensi Anda
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              Masukkan kata kunci untuk mendapatkan rekomendasi film sesuai
+              dengan preferensi Anda.
+            </CardContent>
+          </Card>
         </div>
       )}
     </main>
